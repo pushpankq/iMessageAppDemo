570fd0a5182a9706e63163a050552ccb32598a78
Puspank Kumar <puspankkumar@Pushpanks-MacBook-Air.local>
2019-01-11 18:56:53 +0530

    data upload

---
+++ iMessageAppDemoTests/iMessageAppDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  iMessageAppDemoTests.swift
+//  iMessageAppDemoTests
+//
+//  Created by Puspank Kumar on 08/01/19.
+//  Copyright © 2019 Puspank Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import iMessageAppDemo
+
+class iMessageAppDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
