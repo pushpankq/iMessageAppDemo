7dc16d6a81aca623b7c9616b95315182f696d300
Puspank Kumar <puspankkumar@Pushpanks-MacBook-Air.local>
2019-01-11 19:05:37 +0530

    add printing command

--- iMessageAppDemo/ViewController.swift
+++ iMessageAppDemo/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print("Hello this is pushpank")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
